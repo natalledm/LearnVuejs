03f31f3ae59294e0026fced7ad016ae50e7f281c
Nat <natalledm@nat.note>
2019-10-26 17:23:38 +0200

    :construction: add responsiviness for header

--- cupcake-project/src/components/TheHeader.vue
+++ cupcake-project/src/components/TheHeader.vue
@@ -1,11 +1,8 @@
 <template>
-    <header>
-        <div class="header-container">
-            <img src="../assets/cupcake_cookies.png" alt="logo" class="logo">
-            <router-link to="/" class="logo"></router-link>
-            <nav>
+    <header class="header">
+            <router-link to="/" class="logo"><img src="../assets/cupcake_cookies.png" alt="logo" class="logo"></router-link>
+            <nav class="nav">
                 <ul>
-                    <router-link to="/">Home</router-link>
                     <li><router-link to="/"><img src="../assets/Icons/house.png" class="fas fa-home fa-fw" alt="home"></router-link></li>
                     <li><router-link to="/"><img src="../assets/Icons/compass-arrow.png" class="fas fa-location-arrow fa-fw" alt="location"></router-link></li>
                     <li><router-link to="/"><img src="../assets/Icons/file.png" class="fas fa-bars fa-fw" alt="bars"></router-link></li>
@@ -13,7 +10,6 @@
                     <li><router-link to="/"><img src="../assets/Icons/person.png" class="fas fa-user-alt fa-fw" alt="login" data-toggle="modal" data-target="#exampleModal"></router-link></li>
                     </ul>
             </nav>
-        </div>
     </header>
 </template>
 
@@ -23,34 +19,58 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
-.header-container {
-    display: flex;
-    flex-wrap: nowrap;
+.header {
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    grid-gap: 20px;
+    padding: 10px;
 }
 
 nav {
-    list-style-type: none;
-    width: 300px;
-    padding-right: 20px;
-    align-self: center;
-}
-
-nav ul {
-    padding-inline-start: 0;
+    width: 100%;
+    ul {
+        padding: 0px;
+        margin: 0px;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-gap: 10px;
+    }
 }
 
 nav li {
-    display: inline-block;
-    padding: 5%;
+    list-style-type: none;
+    padding-inline-start: 0;
     background-color: rgb(238, 36, 97);
     border-radius: 50%;
 }
 
-nav li a {
-    color: white;
+@media (min-width: 768px) and (max-width: 1023px) {
+    // tablet
+    .header {
+        grid-template-rows: 1fr;
+        grid-template-columns: 1fr 2fr;
+        padding: 10px;
+    }
 }
 
+@media (min-width: 1024px) {
+    .header {
+        grid-template-areas: 
+            'logo . menu';
+        grid-template-rows: 1fr;
+        grid-template-columns: auto 1fr auto;
+        padding: 20px;
+    }
+
+    .logo {
+        grid-area: logo;
+    }
+
+    .nav {
+        grid-area: menu;
+    }
+}
 
 </style>
