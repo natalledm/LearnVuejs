ede00b38a837420974966b8a760ef7144648946b
Nat <natalledm@nat.note>
2019-10-26 16:13:59 +0200

    Create background and responsive media

--- cupcake-project/src/App.vue
+++ cupcake-project/src/App.vue
@@ -21,24 +21,37 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+body {
+  background: #f6c5d4 url("assets/background.jpg") 0 0 repeat;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  width: 90%;
+  margin: 10px auto;
+  background: #ffff;
+  box-shadow: #666666 0px 0px 7px;
+  overflow: hidden;
+  border-radius: 10px;
+  padding: 0 10px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media (min-width: 768px) and (max-width: 1023px) {
+  //tablet
+  #app {
+    width: 80%;
+    margin: 15px auto;
+    padding: 0 15px;
   }
 }
+
+@media (min-width: 1024px) {
+  //desktop and tv
+  #app {
+    width: 1000px;
+    margin: 20px auto;
+    padding: 0 20px;
+  }
+}
+
 </style>
--- cupcake-project/src/components/TheHeader.vue
+++ cupcake-project/src/components/TheHeader.vue
@@ -1,17 +1,16 @@
 <template>
     <header>
         <div class="header-container">
-            <img src="static/logo.jpg" alt="logo" class="logo">
+            <img src="../assets/cupcake_cookies.png" alt="logo" class="logo">
             <router-link to="/" class="logo"></router-link>
-            <h1 class="header-title titles">CUPCAKES<br/><span class="ampersand">&</span>COOKIES</h1>
             <nav>
                 <ul>
                     <router-link to="/">Home</router-link>
-                    <li><router-link to="/"><i class="fas fa-home fa-fw" alt="home">Home</i></router-link></li>
-                    <li><router-link to="/"><i class="fas fa-location-arrow fa-fw" alt="location">Location</i></router-link></li>
-                    <li><router-link to="/"><i class="fas fa-bars fa-fw" alt="bars">Bars</i></router-link></li>
-                    <li><router-link to="/"><i class="fas fa-question fa-fw" alt="faq">FAQ</i></router-link></li>
-                    <li><router-link to="/"><i class="fas fa-user-alt fa-fw" alt="login" data-toggle="modal" data-target="#exampleModal">Login</i></router-link></li>
+                    <li><router-link to="/"><img src="../assets/Icons/house.png" class="fas fa-home fa-fw" alt="home"></router-link></li>
+                    <li><router-link to="/"><img src="../assets/Icons/compass-arrow.png" class="fas fa-location-arrow fa-fw" alt="location"></router-link></li>
+                    <li><router-link to="/"><img src="../assets/Icons/file.png" class="fas fa-bars fa-fw" alt="bars"></router-link></li>
+                    <li><router-link to="/"><img src="../assets/Icons/question-mark.png" class="fas fa-question fa-fw" alt="faq"></router-link></li>
+                    <li><router-link to="/"><img src="../assets/Icons/person.png" class="fas fa-user-alt fa-fw" alt="login" data-toggle="modal" data-target="#exampleModal"></router-link></li>
                     </ul>
             </nav>
         </div>
@@ -25,5 +24,33 @@ export default {
 </script>
 
 <style>
+
+.header-container {
+    display: flex;
+    flex-wrap: nowrap;
+}
+
+nav {
+    list-style-type: none;
+    width: 300px;
+    padding-right: 20px;
+    align-self: center;
+}
+
+nav ul {
+    padding-inline-start: 0;
+}
+
+nav li {
+    display: inline-block;
+    padding: 5%;
+    background-color: rgb(238, 36, 97);
+    border-radius: 50%;
+}
+
+nav li a {
+    color: white;
+}
+
 
 </style>
